dd9f81c37894ef8cbcb7521716ece6d1b75123f7
wrynnme <yanaworavat@outlook.com>
2023-01-11 16:25:10 +0700

    edit host

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.use(express.json());
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
 
-const PORT = 5000;;
+const PORT = 80;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
